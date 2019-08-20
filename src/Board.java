dd8c24b2d7d7356e0e729120f9b2c956d9e05ac5
Chris Downing <cdowning94@hotmail.com>
2019-08-19 20:20:35 -0700

    Setup git repo in Eclipse.

--- src/Board.java
+++ src/Board.java
@@ -4,62 +4,129 @@ public class Board {
 
 	private final int WIDTH = 7;
 	private final int HEIGHT = 6;
-	private int[][] board;
-	// 1 for X
-	// 0 for empty
-	// -1 for Oh
-	
+	private final int NUM_IN_ROW = 4;
+	private char[][] board;
+	private final char STARTING_PLAYER = 'X';
+	// if INT: 1 for X, zero for empty, -1 for O
 	private char turn;
+	private boolean vsCPU;
 	
+	private int[] workingBoard; 
+	// holds height of available move, 
+	// == HEIGHT then full
 	
 	public Board() {
-		board = new int[HEIGHT][WIDTH];
+		board = new char[HEIGHT][WIDTH];
+		makeBoardEmpty();
+		turn = STARTING_PLAYER;
+		vsCPU = false;
+		workingBoard = new int[WIDTH];
 	}
 	
+	private void makeBoardEmpty() {
+		for(int i = HEIGHT - 1; i >= 0; i--) {
+			for(int j = 0; j < WIDTH; j++) {
+				board[i][j] = ' ';
+			}
+		}	
+	}
+
 	public boolean isOver() {
-		return true;
+		return checkVertical() || checkHorizontal() 
+				|| checkR_Diag() || checkL_Diag() 
+				|| boardIsFull();
 	}
 	
+	private boolean boardIsFull() {
+		for(int i = 0; i < WIDTH; i++) {
+			if(workingBoard[i] >= HEIGHT) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean checkVertical() {
+		char check;
+		// uhhhhhh
+		for(int i = 0; i < WIDTH; i++) {
+			check = board[0][i];
+			
+		}
+		return false;
+	}
+
+	private boolean checkHorizontal() {
+		return false;
+	}
+
+	private boolean checkR_Diag() {
+		return false;
+	}
+
+	private boolean checkL_Diag() {
+		return false;
+	}
+
 	public char whoseTurn() {
 		return turn;
 	}
 
-	/*
-	 * User move
-	 * input is 1-7 column indexing, array is columned at 0-6
-	 */
-	public void makeMove(int move) {
-		move--;
-		int i = 0;
+	public boolean makeMove(int move) {
+		move--; // move to zero-based indexing
 		
-		while (board[i][move] != 0) {
-			i++;
+		//	validate move
+		if(!isValidMove(move)) {
+			return false;
 		}
 		
-		try {
-			board[i][move] = markBoard();
-		} catch(ArrayIndexOutOfBoundsException e) {
-			// throw exception to main.java
-			throw e;
-		}
+		// update the board
+		updateBoard(move);
 		
+		// check for winner, do I need this here?
+		// No, that's the while loop in Driver...
+		
+		// update turn
+		turn = (turn == 'X') ? 'O' : 'X';
+		
+		// print board
+		displayBoard();
+		
+		return true;
 	}
 
-	/*
-	 * CPU move
-	 */
-	public void makeMove() {
+	private void displayBoard() {
+		for(int i = HEIGHT - 1; i >= 0; i--) {
+			for(int j = 0; j < WIDTH; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+
+	private void updateBoard(int move) {
+		// where will the piece end up
+		int col = workingBoard[move];
+		// mark board
+		board[col][move] = turn;
+		// update relevant column
+		workingBoard[move]++;
+	}
+
+	private boolean isValidMove(int row) {
+		
+		if(row > WIDTH - 1) {
+			return false; // out of bounds
+		}
+		return workingBoard[row] < HEIGHT;
+	}
+
+	// Used for single player games
+	public void cpuMove() {
 		Random r = new Random();
 		int col = r.nextInt(6);
-		makeMove(col);
+		//makeMove(col);
 		
-	}
-	
-	private int markBoard() {
-		if(turn == 'X')
-			return 1;
-		
-		return 0;
 	}
 	
 	
