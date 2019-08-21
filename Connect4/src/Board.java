5126e3ed37c39d429002779ab791c5a8032fae0f
Chris Downing <cdowning94@hotmail.com>
2019-08-20 20:41:11 -0700

    Some win conditions done, I/O coming along.
    Vertical win condition done. Shows winner properly. Full board ends
    session.
    
    Need to think about scoring and how to check for possible winning
    scenarios. This will contribute to more efficient win checking also.

--- Connect4/src/Board.java
+++ Connect4/src/Board.java
@@ -6,24 +6,30 @@ public class Board {
 	private final int HEIGHT = 6;
 	private final int NUM_IN_ROW = 4;
 	private char[][] board;
-	private final char STARTING_PLAYER = 'X';
+	private final Character STARTING_PLAYER = 'X';
 	// if INT: 1 for X, zero for empty, -1 for O
-	private char turn;
-	private boolean vsCPU;
+	private Character turn;
 	
+
+	private boolean vsCPU = false;
+	
+	// tracks most recent move (zero-based column)
+	private int lastMove;
+	
+	// holds height of available move, == HEIGHT is full column
 	private int[] workingBoard; 
-	// holds height of available move, 
-	// == HEIGHT then full
 	
 	public Board() {
 		board = new char[HEIGHT][WIDTH];
-		makeBoardEmpty();
+		createEmptyBoard();
+		//turn = STARTING_PLAYER;
 		turn = STARTING_PLAYER;
-		vsCPU = false;
 		workingBoard = new int[WIDTH];
+		
+		lastMove = -1;
 	}
 	
-	private void makeBoardEmpty() {
+	private void createEmptyBoard() {
 		for(int i = HEIGHT - 1; i >= 0; i--) {
 			for(int j = 0; j < WIDTH; j++) {
 				board[i][j] = ' ';
@@ -32,28 +38,43 @@ public class Board {
 	}
 
 	public boolean isOver() {
+		if(lastMove == -1) {
+			return false;
+		}
+		
 		return checkVertical() || checkHorizontal() 
-				|| checkR_Diag() || checkL_Diag() 
+				/*|| checkR_Diag() || checkL_Diag() */
 				|| boardIsFull();
 	}
 	
 	private boolean boardIsFull() {
 		for(int i = 0; i < WIDTH; i++) {
-			if(workingBoard[i] >= HEIGHT) {
-				return true;
+			if(workingBoard[i] < HEIGHT) {
+				return false;
 			}
 		}
-		return false;
+		turn = null;
+		return true;
 	}
 
+	// FIXME
 	private boolean checkVertical() {
-		char check;
-		// uhhhhhh
-		for(int i = 0; i < WIDTH; i++) {
-			check = board[0][i];
-			
+		int lastMoveRow = workingBoard[lastMove] - 1;
+		System.out.println("lastMoveRow: " + lastMoveRow);
+		
+		if(lastMoveRow < NUM_IN_ROW - 1) {
+			// not enough pieces in column
+			System.out.println("Not enough pieces to win vertically.");
+			return false;
 		}
-		return false;
+		for(int i = 0; i < NUM_IN_ROW; i++) {
+			if(board[lastMoveRow - i][lastMove] == turn) {
+				System.out.println("space on board: " + board[lastMoveRow - i][lastMove]);
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 	private boolean checkHorizontal() {
@@ -68,31 +89,41 @@ public class Board {
 		return false;
 	}
 
-	public char whoseTurn() {
+	// changed return type
+	public Character whoseTurn() {
 		return turn;
 	}
 
+	/**
+	 * Receives a row for a new piece to be put in to (1-WIDTH)
+	 * Immediately switches to 0-(WIDTH-1) for processing
+	 */
 	public boolean makeMove(int move) {
-		move--; // move to zero-based indexing
+		move--; // change move to zero-based indexing
 		
-		//	validate move
+		//	validate move, end the turn.
 		if(!isValidMove(move)) {
 			return false;
 		}
 		
 		// update the board
 		updateBoard(move);
+		lastMove = move;
 		
 		// check for winner, do I need this here?
 		// No, that's the while loop in Driver...
 		
 		// update turn
-		turn = (turn == 'X') ? 'O' : 'X';
+		swapTurn();
 		
 		// print board
 		displayBoard();
 		
 		return true;
+	}
+
+	public void swapTurn() {
+		turn = (turn == 'X') ? 'O' : 'X';		
 	}
 
 	private void displayBoard() {
@@ -104,11 +135,12 @@ public class Board {
 		}
 	}
 
+	// row/col semantics here might be backwards.
 	private void updateBoard(int move) {
 		// where will the piece end up
-		int col = workingBoard[move];
+		int row = workingBoard[move];
 		// mark board
-		board[col][move] = turn;
+		board[row][move] = turn;
 		// update relevant column
 		workingBoard[move]++;
 	}
--- Connect4/src/Driver.java
+++ Connect4/src/Driver.java
@@ -9,9 +9,12 @@ public class Driver {
 		// stuff
 		
 		int move;
+		
+		welcome();
+		
 		do {
 		
-			System.out.print("Enter a row: ");
+			System.out.print("\nEnter a row: ");
 			move = keyb.nextInt();
 			if(!game.makeMove(move)) {
 				System.out.println("Invalid move.");
@@ -19,9 +22,21 @@ public class Driver {
 			
 		} while(!game.isOver());
 		
-		System.out.println("The game is over!");
+		System.out.println("\nThe game is over!");
+		
+		// accounts for full board with ties
+		if(game.whoseTurn() != null) {
+			game.swapTurn(); // FIXME because makeMove changes turn before isOver is called
+			System.out.println(game.whoseTurn() + " has won!");
+		} else {
+			System.out.println("The board has been filled. Tie game!");
+		}
 		
 		keyb.close();
+	}
+
+	private static void welcome() {
+		System.out.println("Yooooo, it's Connect 4.");		
 	}
 	
 }
